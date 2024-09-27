f31b71d2f9308541893925ee6c136ed38afe94ac
SanjoDB <1006110438@u.icesi.edu.co>
2024-09-27 02:35:26 -0500

    Endpoints added

---
+++ src/content/content.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ContentService } from './content.service';
+import { CreateContentDto } from './dto/create-content.dto';
+import { UpdateContentDto } from './dto/update-content.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { ValidRoles } from '../auth/interfaces/valid-roles';
+
+@Controller('api/v1/content')
+export class ContentController {
+
+  constructor(private readonly contentService: ContentService) {}
+
+  @Get()
+  findAll() {
+
+    return this.contentService.findAll();
+
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+
+    return this.contentService.findOne(id);
+
+  }
+
+  @Post()
+  @Auth(ValidRoles.admin) // Solo admin puede crear contenido
+  create(@Body() createContentDto: CreateContentDto) {
+
+    return this.contentService.create(createContentDto);
+
+  }
+
+  @Patch(':id')
+  @Auth(ValidRoles.admin) // Solo admin puede actualizar contenido
+  update(@Param('id') id: string, @Body() updateContentDto: UpdateContentDto) {
+
+    return this.contentService.update(id, updateContentDto);
+
+  }
+
+  @Delete(':id')
+  @Auth(ValidRoles.admin) // Solo admin puede eliminar contenido
+  remove(@Param('id') id: string) {
+
+    return this.contentService.remove(id);
+
+  }
+  
+}
