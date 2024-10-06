d14710d2fa0d67411e7babf4b2a35abe4250cc1b
SanjoDB <1006110438@u.icesi.edu.co>
2024-10-06 16:49:53 -0500

    Error launching fixed

--- src/reactions/reactions.module.ts
+++ src/reactions/reactions.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReactionsService } from './reactions.service';
 import { ReactionsController } from './reactions.controller';
 import { Reaction } from './entities/reaction.entity';
+import { User } from 'src/auth/entities/user.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reaction])],
+  imports: [TypeOrmModule.forFeature([Reaction, User, Comment]), AuthModule],
   controllers: [ReactionsController],
   providers: [ReactionsService],
   exports: [ReactionsService],
