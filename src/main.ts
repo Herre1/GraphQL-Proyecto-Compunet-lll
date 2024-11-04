971e0160636763304d746c8abf5e21c0bde26f71
Manuel Herrera <98495072+Herre1@users.noreply.github.com>
2024-11-04 15:12:51 -0500

    CORS FIX

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS para permitir solicitudes desde otros dominios
+  app.enableCors({
+    origin: 'http://localhost:3000', // Cambia esto a la URL de tu frontend si es diferente
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  // Configuración global de validación de datos
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
